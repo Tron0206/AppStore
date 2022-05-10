aed6c9a994bc7134299f1895ea699b70d3d90432
Tron0206 <88591700+Tron0206@users.noreply.github.com>
2022-05-10 19:53:50 +0300

    Extensions and Helpers for Code

--- Appstore/Controller/AppsSearchController.swift
+++ Appstore/Controller/AppsSearchController.swift
@@ -10,10 +10,11 @@ import UIKit
 
 class AppsSearchController: UICollectionViewController {
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        collectionView.backgroundColor = .red
+        collectionView.register(SearchResultCell.self, forCellWithReuseIdentifier: SearchResultCell.identifier)
     }
     
     init() {
@@ -22,5 +23,20 @@ class AppsSearchController: UICollectionViewController {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SearchResultCell.identifier, for: indexPath)
+        return cell
+    }
+}
+
+extension AppsSearchController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: view.frame.width, height: 350)
     }
 }
